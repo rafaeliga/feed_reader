858afb6d56f346d494a2df25f8f4ab87a26c19c1
Lucas Cartaxo <lucascartaxor@gmail.com>
2013-04-27 13:44:07 -0300

    kinda of getting values from xml

--- assets/javascripts/reader.js
+++ assets/javascripts/reader.js
@@ -1,9 +1,34 @@
 $(function() {
   console.log('READER START');
+
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', "feed_itunes.xml", true);
+  xhr.onload = function(e) {
+
+      var xml = this.response;
+      xmlDoc = $.parseXML( xml );
+      $xml = $( xmlDoc );
+
+      $channel = $xml.find( "channel" )
+
+      $divchannels = $("#channels");
+
+      var appendHtml;
+
+      $channel.find("item").each( function(){
+        appendHtml += $(this).find("title").text() + "<br><br>";
+      });
+      $divchannels.html(appendHtml);
+
+  };
+
+  xhr.send();
 });
 
 function search() {
   $term = $('#term').val();
+
 
   console.log($term)
 }
