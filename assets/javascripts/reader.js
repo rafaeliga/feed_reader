5cdd9e174d07d909919c21f7d65a622c284c3089
Rafael Iga <rafael.f.iga@gmail.com>
2013-04-27 13:45:52 -0300

    reader reading urls

--- assets/javascripts/reader.js
+++ assets/javascripts/reader.js
@@ -1,7 +1,6 @@
 $(function() {
   console.log('READER START');
 
-
   var xhr = new XMLHttpRequest();
   xhr.open('GET', "feed_itunes.xml", true);
   xhr.onload = function(e) {
@@ -24,11 +23,33 @@ $(function() {
   };
 
   xhr.send();
+
+  search();
 });
 
 function search() {
-  $term = $('#term').val();
+  var term = $('#term').val();
 
-
-  console.log($term)
+  $.ajax({
+    type: 'GET',
+    url: term,
+    success: function(data, textStatus, request){
+      var content_type = request.getResponseHeader('Content-Type');
+      console.log(content_type)
+      
+      if (content_type.match(/text\/html/)) {
+        console.log('HTML -> FIND RSS URL');
+        var link_rss = $(data).filter("link[type='application/rss+xml']");
+        var rss_link = link_rss.attr('href');
+        console.log(rss_link)
+      } else if (content_type.match(/xml/)) {
+        console.log('XML -> PARSE');
+        console.log(data)
+      }
+    },
+    error: function (request, textStatus, errorThrown) {
+      console.log(request.getResponseHeader('Content-Type'));
+      console.log('f')
+    }
+  });
 }
